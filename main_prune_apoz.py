a7ed28120efeb105c9f5d6ec0ea9c0577ba3d5a6
Nathan Bailey <nathanbaileyw@gmail.com>
2024-01-18 19:28:30 +0000

    Added APOZ prune files

---
+++ main_prune_apoz.py
@@ -0,0 +1,210 @@
+import torch
+import torchvision
+import sys
+import dataset
+import network_apoz
+import network
+import dataset_apoz
+import train_test
+import torch_pruning as pruning
+import math
+import pytorch_model_summary as pms
+import apoz_prune
+import pickle
+
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+sys.stdout.flush()
+
+mean = torch.Tensor([0.6661, 0.6211, 0.5492]).to('cpu')
+std = torch.Tensor([0.2871, 0.2917, 0.3310]).to('cpu')
+
+model = network_apoz.RecycleNetwork(num_classes=5).to(device)
+model.load_state_dict(torch.load('recycle_net_trained.pt'))
+model.eval()
+
+transforms = torchvision.transforms.Compose([torchvision.transforms.Resize((150,150)), torchvision.transforms.ToTensor(), torchvision.transforms.Normalize(mean, std)])
+
+valid_dataset = dataset_apoz.RecyclingDataset(data_dir='compiled_dataset', dataset_type='valid', data_transforms=transforms)
+validloader = torch.utils.data.DataLoader(valid_dataset, batch_size=32, shuffle=False, pin_memory=True, num_workers=4)
+print(len(validloader))
+
+layer_names = [name for name, _ in model.named_modules()]
+layer_names = [name for name in layer_names.copy() if 'stem.0' in name or 'head.0' in name or 'project_conv' in name or 'expand_conv' in name or 'depthwise_conv' in name or 'se_reduce' in name or 'se_expand' in name]
+# print(layer_names)
+outputs_conv_blocks = None
+outputs_conv_stem = None
+outputs_conv_head = None
+with torch.no_grad():
+    batch = next(iter(validloader))
+    images = batch[0].to(device)
+    outputs, _ = model(images)
+    inter_outputs = [i.cpu() for i in outputs[0] if i is not None]
+    for i, item in enumerate(inter_outputs):
+        if item is not None:
+            globals()['outputs_inner_layer_'+str(i)] = None
+
+collected_outputs = []
+masks = []
+
+prune_percentage = 0.95
+with torch.no_grad():
+    for idx, batch in enumerate(validloader):
+        images = batch[0].to(device)
+        print(batch[1])
+        outputs, _ = model(images)
+        inter_outputs = [i.cpu() for i in outputs[0] if i is not None]
+        if idx == 0:
+            outputs_conv_stem = outputs[1].cpu()
+            outputs_conv_head = outputs[2].cpu()
+            for i, item in enumerate(inter_outputs):
+                if item is not None:
+                    globals()['outputs_inner_layer_'+str(i)] = item
+        else:
+            outputs_conv_stem = torch.cat((outputs_conv_stem, outputs[1].cpu()), dim=0)
+            outputs_conv_head = torch.cat((outputs_conv_head, outputs[2].cpu()), dim=0)
+            for i, item in enumerate(inter_outputs):
+                if item is not None:
+                    globals()['outputs_inner_layer_'+str(i)] = torch.cat((globals()['outputs_inner_layer_'+str(i)], item), dim=0)
+    collected_outputs.append(outputs_conv_stem)
+
+    masks.append(apoz_prune.compute_mask_conv_layer(outputs_conv_stem, prune_percentage))
+    for i, item in enumerate(inter_outputs):
+        if item is not None:
+            collected_outputs.append(globals()['outputs_inner_layer_'+str(i)])
+            # print(apoz_prune.apoz_per_feature_map(globals()['outputs_inner_layer_'+str(i)]))
+            # print(apoz_prune.compute_mask_conv_layer(globals()['outputs_inner_layer_'+str(i)], prune_percentage))
+            prune_idx = [i for i, j in enumerate(apoz_prune.compute_mask_conv_layer(globals()['outputs_inner_layer_'+str(i)], prune_percentage)) if j == 1]
+            # print(prune_idx)
+            # print(len(prune_idx))
+            masks.append(apoz_prune.compute_mask_conv_layer(globals()['outputs_inner_layer_'+str(i)], prune_percentage))
+    collected_outputs.append(outputs_conv_head)
+    masks.append(apoz_prune.compute_mask_conv_layer(outputs_conv_head, prune_percentage))
+
+collected_outputs.reverse()
+masks.reverse()
+layer_names.reverse()
+
+
+def train_network(model, device, epochs):
+    train_dataset = dataset.RecyclingDataset(data_dir='compiled_dataset', dataset_type='train', data_transforms=transforms)
+
+    trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True, pin_memory=True, num_workers=4)
+    
+    valid_dataset = dataset.RecyclingDataset(data_dir='compiled_dataset', dataset_type='valid', data_transforms=transforms)
+    validloader = torch.utils.data.DataLoader(valid_dataset, batch_size=32, shuffle=False, pin_memory=True, num_workers=4)
+    
+    model.to(device)
+    model.train()
+    for param in model.parameters():
+        param.requires_grad=True
+    
+    loss = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(filter(lambda param: param.requires_grad, model.parameters()), lr=0.00001, momentum=0.9, weight_decay=5e-2)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer, factor=0.1, mode='min', patience=3, min_lr=0.00000000001, threshold_mode='abs', threshold=1e-2, verbose=True)
+
+    model = train_test.train_network(model=model, num_epochs=epochs, optimizer=optimizer, loss_function=loss, trainloader=trainloader, validloader=validloader, device=device, scheduler=scheduler)
+    return model
+
+def prune_apoz_train(model, idx_to_prune, num, epochs):
+    model.eval()
+    model.to(device)
+    conv_idx = apoz_prune.compute_mask_conv_layer(collected_outputs[idx_to_prune], num).nonzero()
+    prune_idx = conv_idx.reshape(conv_idx.size(0)).tolist()
+    with open('idx_'+str(idx_to_prune)+'.pkl', 'wb') as f:
+        pickle.dump(prune_idx, f)
+    named_layer = layer_names[idx_to_prune]
+    layer_to_use = None
+    for name, layer in model.named_modules():
+        if name == named_layer:
+            layer_to_use = layer
+    DG = pruning.DependencyGraph()
+    DG.build_dependency(model, example_inputs=torch.randn(1,3,150,150).to(device))
+    if layer_to_use.groups == layer_to_use.in_channels:
+        print('Depthwise Detected')
+        group = DG.get_pruning_group(layer_to_use, pruning_fn=pruning.prune_depthwise_conv_out_channels, idxs=prune_idx)
+        if DG.check_pruning_group(group):
+            group.prune()
+    else:
+        group = DG.get_pruning_group(layer_to_use, pruning_fn=pruning.prune_conv_out_channels, idxs=prune_idx)
+        if DG.check_pruning_group(group):
+            group.prune()
+    print(group)
+    model.to(device)
+    # pms.summary(model, torch.zeros((1, 3, 150,150)).to(device), show_input=False, print_summary=True, max_depth=5, show_parent_layers=True)
+    model = train_network(model, device, epochs)
+    return model
+
+
+model = network.RecycleNetwork(num_classes=5).to(device)
+model.load_state_dict(torch.load('recycle_net_trained.pt'))
+model.train()
+
+#11 prunes
+
+DG = pruning.DependencyGraph()
+DG.build_dependency(model, example_inputs=torch.randn(1,3,150,150).to(device))
+model.to(device)
+num_conv_layers_pruned = 0
+train_model=True
+idx_conv = 0
+num_prunes = 0
+
+num_pruned = 0
+
+
+for idx, (name,layer) in enumerate(model.named_modules()):
+    if isinstance(layer, torch.nn.Conv2d):
+        num_conv_layers_pruned +=1
+        train_model=True
+        prune_idx = masks[len(masks)-idx_conv-1]
+        # print(prune_idx.size())
+        prune_idx = [i for i, j in enumerate(prune_idx) if j == 1]
+        name = layer_names[len(layer_names)-idx_conv-1]
+        print(name)
+        # print(prune_idx)
+        print(len(prune_idx))
+        num_pruned += len(prune_idx)
+        # print(layer.weight.size())
+        if layer.groups == layer.in_channels:
+            print('Depthwise Detected')
+            group = DG.get_pruning_group(layer, pruning_fn=pruning.prune_depthwise_conv_out_channels, idxs=prune_idx)
+            if DG.check_pruning_group(group):
+                group.prune()
+        else:
+            group = DG.get_pruning_group(layer, pruning_fn=pruning.prune_conv_out_channels, idxs=prune_idx)
+            if DG.check_pruning_group(group):
+                group.prune()
+        print(group)
+        for i, (dep, idxs) in enumerate(group):
+            if isinstance(dep.target.module, torch.nn.Conv2d) and 'out_channels' in str(dep.handler):
+                print(str(dep.handler))
+                print(dep.target._name)
+                idx_name = layer_names.index(dep.target._name)
+                print(idx_name)
+                mask = torch.tensor([(1 if i not in prune_idx else 0) for i in range(masks[idx_name].size()[0])]).type('torch.BoolTensor')
+                print(mask)
+                print(masks[idx_name].size())
+                masks[idx_name] = torch.masked_select(masks[idx_name], mask)
+                print(masks[idx_name].size())
+        idx_conv += 1
+    if num_conv_layers_pruned % 3 == 0 and num_conv_layers_pruned != 0 and train_model:
+        print('Starting Training Cycle')
+        pms.summary(model, torch.zeros((1, 3, 150,150)).to(device), show_input=False, print_summary=True, max_depth=5, show_parent_layers=True)
+        #model = train_network(model, device=device, epochs=100)
+        print('Ended Training Cycle')
+        train_model=False
+
+print(num_pruned)
+
+test_dataset = dataset.RecyclingDataset(data_dir='compiled_dataset', dataset_type='test', data_transforms=transforms)
+testloader = torch.utils.data.DataLoader(test_dataset, batch_size=32, shuffle=False, pin_memory=True, num_workers=4)
+pms.summary(model, torch.zeros((1, 3, 150,150)).to(device), show_input=False, print_summary=True, max_depth=5, show_parent_layers=True)
+#Train and testing
+model.train()
+model.to(device)
+num_epochs = 100
+model = train_network(model, device=device, epochs=num_epochs)
+test_loss = torch.nn.CrossEntropyLoss()
+model.eval()
+train_test.test_network(model=model, testloader=testloader, loss_function=test_loss, device=device)
---
+++ train_test.py
@@ -0,0 +1,97 @@
+import torch
+import sys
+import numpy as np
+from early import pytorchtools
+
+def calculate_accuracy(outputs, ground_truth):
+    softmaxed_output = torch.nn.functional.softmax(outputs, dim=1)
+    predictions = torch.argmax(softmaxed_output, dim=1)
+    num_correct = torch.sum(torch.eq(predictions, ground_truth)).item()
+    return num_correct, ground_truth.size(0)
+
+def train_network(model, num_epochs, optimizer, loss_function, trainloader, validloader, device, patience=10, path_to_model='recycle_net', scheduler=None):
+    print('Training Started')
+    sys.stdout.flush()
+    early_stop = pytorchtools.EarlyStopping(patience=patience, verbose=True,path=path_to_model+'.pt')
+    for epoch in range(1, num_epochs+1):
+        train_loss = []
+        valid_loss = []
+        num_correct_train = 0
+        num_examples_train = 0
+        num_correct_valid = 0
+        num_examples_valid = 0
+        model.train()
+        for batch in trainloader:
+            optimizer.zero_grad()
+            images = batch[0].to(device)
+            labels = batch[1].to(device)
+            outputs = model(images)
+            loss = loss_function(outputs, labels)
+            #Calculate the gradients for parameters (backprop)
+            loss.backward()
+            #Update the values of the parameter using the optimizer
+            optimizer.step()
+            train_loss.append(loss.item())
+            num_corr, num_ex = calculate_accuracy(outputs, labels)
+            num_examples_train += num_ex
+            num_correct_train += num_corr
+        
+        model.eval()
+        with torch.no_grad():
+            for batch in validloader:
+                images = batch[0].to(device)
+                labels = batch[1].to(device)
+                outputs = model(images)
+                loss = loss_function(outputs, labels)
+                valid_loss.append(loss.item())
+                num_corr, num_ex = calculate_accuracy(outputs, labels)
+                num_examples_valid += num_ex
+                num_correct_valid += num_corr
+                
+        
+        print('Epoch: {}, Training Loss: {:4f}, Validation Loss: {:4f}, Training Accuracy: {:4f}, Validation Accuracy: {:4f}'.format(epoch, np.mean(train_loss), np.mean(valid_loss), num_correct_train/num_examples_train, num_correct_valid/num_examples_valid))
+        
+        if scheduler:
+            scheduler.step(np.mean(train_loss))
+        
+        if early_stop.counter == 0:
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'scheduler': scheduler.state_dict(),
+                'validation_loss': np.mean(valid_loss),
+                'train_loss': np.mean(train_loss)
+                }, 'full_model.tar')
+            torch.save(model, 'model.pth')
+        
+        early_stop_loss = np.mean(train_loss)
+        early_stop(early_stop_loss, model)
+        
+        if early_stop.early_stop:
+            print('Early Stopping at Epoch: {}'.format(epoch))
+            break
+    sys.stdout.flush()
+            
+    model.load_state_dict(torch.load(path_to_model+'.pt'))
+    model.to(device)
+    return model
+
+
+def test_network(model, testloader, loss_function, device):
+    test_loss = []
+    num_examples = 0
+    num_correct = 0
+    model.eval()
+    with torch.no_grad():
+        for batch in testloader:
+            images = batch[0].to(device)
+            labels = batch[1].to(device)
+            output = model(images)
+            loss = loss_function(output, labels)
+            test_loss.append(loss.item())
+            num_corr, num_ex = calculate_accuracy(output, labels)
+            num_examples += num_ex
+            num_correct += num_corr
+
+    print('Test Loss: {:4f}, Test Accuracy: {:.4f}'.format(np.mean(test_loss), num_correct/num_examples))
