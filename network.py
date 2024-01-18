9451bb6e86418602cffb5bdd8f631e2282260ba2
Nathan Bailey <nathanbaileyw@gmail.com>
2024-01-18 19:52:47 +0000

    Added Taylor Pruning Work

--- network.py
+++ network.py
@@ -13,10 +13,11 @@ class RecycleNetwork(torch.nn.Module):
         model_name = 'efficientnet_lite4'
         self.backbone = build_efficientnet_lite(model_name, 1000)
         self.backbone.load_pretrain('./EfficientNet/efficientnet_lite4.pth')
-        self.backbone.fc = torch.nn.Linear(1280, num_classes)
+        self.backbone.fc = torch.nn.Linear(1280, 5)
 
     def forward(self, x):
         return self.backbone(x)
 
-# model = RecycleNetwork(5)
-# pms.summary(model, torch.zeros((1, 3, 150,150)), show_input=False, print_summary=True, max_depth=5, show_parent_layers=True)
+model = RecycleNetwork(5)
+print(model)
+#pms.summary(model, torch.zeros((1, 3, 150,150)), show_input=False, print_summary=True, max_depth=5, show_parent_layers=True)
